386ebf480fcc9dd2d917ec512b4a2b949d86d74f
ChengHsuanLiu <chenghsuango@gmail.com>
2016-08-19 19:57:58 +0800

    add columns to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818105721) do
+ActiveRecord::Schema.define(version: 20160818145508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 20160818105721) do
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "excerpt"
+    t.datetime "deleted_at"
   end
 
   create_table "tags", force: :cascade do |t|
