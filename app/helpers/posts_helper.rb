ea66b5fc344cf56bfacbc6c9fa1e0e9bdc071f60
ChengHsuanLiu <chenghsuango@gmail.com>
2016-09-04 20:30:37 +0800

    add prev next post link to post show

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -6,4 +6,20 @@ module PostsHelper
       content_tag :p, 'Private'
     end
   end
+
+  def prev_post_link(post_id)
+    @post = Post.find(post_id)
+    @prev_post = @post.prev_post
+    if @prev_post.present?
+      link_to "Previous - #{@prev_post.title}", post_path(@prev_post)
+    end
+  end
+
+  def next_post_link(post_id)
+    @post = Post.find(post_id)
+    @next_post = @post.next_post
+    if @next_post.present?
+      link_to "Next - \n#{@prev_post.title}", post_path(@next_post)
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,14 @@ class Post < ApplicationRecord
     self.save!
   end
 
+  def next_post
+    @next_post = Post.where("id > ?", self.id).first
+  end
+
+  def prev_post
+    @prev_post = Post.where("id < ?", self.id).first
+  end
+
   def self.available_posts
     public_posts.exist_posts
   end
